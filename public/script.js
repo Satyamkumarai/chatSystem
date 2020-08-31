8d545407c89bf9fc393e669ddc190d8c22659265
Satyamkumarai <satyamkumarai@gmail.com>
2020-08-31 09:09:35 +0530

    Auto scorrll

--- public/script.js
+++ public/script.js
@@ -104,5 +104,6 @@ function appendMessageString(message,other=false){
         messageElement.classList.add('other')
     messageElement.innerHTML=message;
     messageContainer.append(messageElement);
+    messageElement.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
 }
 ///---------------------------------------------------------
